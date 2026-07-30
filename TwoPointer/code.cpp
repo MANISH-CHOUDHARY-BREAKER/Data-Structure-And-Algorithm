68a8f8135cc6aac83667fce42172b0e27b6e9982
Manish Choudhary <mc1124400@gmail.com>
2026-07-31 02:43:06 +0530

    dsa: add Two Sum II solution using two pointers

---
+++ TwoPointer/code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+//function definition
+
+void printHello() {
+    cout << "hello\n";
+
+}
+
+int main() {
+    //function call
+    printHello();
+    return 0;
+}
