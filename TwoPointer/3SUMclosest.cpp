b27086b71baafad6c4a239b1dad8be3394d4eb52
Manish Choudhary <mc1124400@gmail.com>
2026-08-09 19:18:16 +0530

    3 SumClosest

--- TwoPointer/3SUMclosest.cpp
+++ TwoPointer/3SUMclosest.cpp
@@ -11,7 +11,7 @@ int threeSumClosest(vector<int>& nums, int target){
     sort(nums.begin(), nums.end());
     int result = nums[0] + nums[1] + nums[2];
 
-    for(int i = 0; i<n-2; i++) {
+    for(int i = 0; i<n-2; i++) {  
         int left = i +1;
         int right = n - 1;
 
